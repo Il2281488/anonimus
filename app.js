af3de65faf83cc60b9794f22f2faa5c98540424d
Elias <ilya.alekseevich.00@list.ru>
2023-07-03 20:41:17 +0300

    do form

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ------------------------- TOGGLE PASSWORD
 
-$('#togglePassword').click(function() {
+$('#togglePassword').click(function () {
   if ($('#password').prop('type') === 'password') {
     $('#password').prop('type', 'text');
     $('#togglePassword').html('<i class="bi bi-eye-slash"></i>');
@@ -10,3 +10,19 @@ $('#togglePassword').click(function() {
   }
 });
 
+
+
+$(document).ready(function () {
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    if ($('#password').prop('type') === 'text') {
+      $('#password').prop('type', 'password');
+      alert('данные украдены');
+    } else {
+      alert('данные украдены');
+    }
+    this.reset();
+  });
+
+});
